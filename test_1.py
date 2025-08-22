91aad5c6540cf3e03b1877023a31c4ae140221a3
PORaven@Linux <SEMROM1917@outlook.com>
2025-08-22 17:35:27 +0500

    0.1

--- test_1.py
+++ test_1.py
@@ -1,3 +1,10 @@
+from abc import ABC, abstractstaticmethod, abstractproperty
+from random import randint
+import math
+from pprint import pprint
+from typing import Collection, List, Dict
+
+
 class Test(Exception):
     def __init__(
         self, *args: object, description: str = "Hello, test pre-commit"
@@ -8,9 +15,20 @@ class Test(Exception):
     def __repr__(self) -> str:
         return f"{self.description}"
 
-    def add_object_property(self, option: dict[str | int, str | int]) -> None:
+    def add_object_property(
+        self, option: dict[str | int, str | int]
+    ) -> "Test":  # возвращаем self, исправлена аннотация
         self.properties = option
         return self
 
-    def convert_abs(num: int) -> int:
-        return abs(num)
+    @staticmethod
+    def convert_abs(num: int) -> None:  # исправлен метод на статический
+        print(abs(num))
+
+
+def h() -> None:
+    print("Hello")
+
+
+def ppr() -> None:
+    pprint(randint(1, 10))  # исправлено использование math
